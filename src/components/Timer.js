7b19d43283207e31107ba36274e16ad89d03e3ef
tanghaoran <407458281@qq.com>
2020-10-20 21:13:37 +0800

    fix:change timer style

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,65 +1,72 @@
 import React from "react";
-import { NavLink} from 'react-router-dom';
-import './Timer.css';
+import { NavLink } from "react-router-dom";
+import "./Timer.css";
 
-class Timer extends React.Component{
-    state={
-        time:0,
-        start: false,
-        end: false
-    }
-  
-    
-    handleChange= (event) => {
-        event.preventDefault();
-        this.setState(
-          {time: event.target.value}
-          )
+class Timer extends React.Component {
+  state = {
+    time: 0,
+    start: false,
+    end: false,
+  };
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ time: event.target.value });
+  };
+  reduce = () => {
+    this.setState({
+      time: this.state.time - 1,
+    });
+  };
+
+  timerStart = () => {
+    const timer = setInterval(() => {
+      if (this.state.time <= 1) {
+        this.setState({
+          end: true,
+        });
+      } else {
+        this.setState({
+          time: this.state.time - 1,
+        });
       }
-    reduce=() => {
-        this.setState({
-            time: this.state.time-1,
-        })
-    }
-    
-    timerStart = () => {
-        const timer = setInterval(() => {
-            if(this.state.time<=1){
-                this.setState({
-                    end:true
-                })
-            }else{
-            this.setState({
-                time: this.state.time -1
-            })}
-        },1000)
+    }, 1000);
 
-        this.setState({
-            start: true,
-            timer
-        })
-    }
+    this.setState({
+      start: true,
+      timer,
+    });
+  };
 
-    render(){
-        return <div className='timerBackground'>
-            <h2>在线倒计时器</h2>
-            <div className="timerPad">
-                设置时间 <input type="text" name="time" onChange={this.handleChange} className='input' />
-                <button className='start' onClick={this.timerStart} >Start</button>
-            </div>
-
-            <div className='timeWindow'>
-                {
-                    this.state.start?
-                    this.state.end?
-                    "End":`${this.state.time} Seconds`
-                    :"Start"
-                }
-            </div>
-
-        
+  render() {
+    return (
+      <div className="timerBackground">
+        <h1 className="timer-title">在线倒计时器</h1>
+        <div className="timerPad">
+          <div className="timerPad-up">
+            设置时间
+            <input
+              type="text"
+              name="time"
+              onChange={this.handleChange}
+              className="input"
+            />
+          </div>
+          <button className="start" onClick={this.timerStart}>
+            Start
+          </button>
         </div>
-    }
+
+        <div className="timeWindow">
+          {this.state.start
+            ? this.state.end
+              ? "End"
+              : `${this.state.time} Seconds`
+            : "Start"}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Timer;
